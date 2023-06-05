c2f8f888d3a5df9fd56a633a7d114b5187c9fd53
Atthie Mulongo <atthie27@gmail.com>
2023-06-05 22:46:49 +0200

    connexio à la BDD

--- conf/database.jsx
+++ conf/database.jsx
@@ -1,17 +1,15 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("Erp", "postgres", "admin", {
-  host: "localhost",
-  dialect: "postgres",
+const {Sequelize} = require('sequelize');
+const sequelize = new Sequelize('Erp','postgres','admin',{
+    host:'localhost',
+    dialect:'postgres',
 });
 
-sequelize
-  .sync()
-  .then(() => {
-    console.log("salut  tu es connecté à la bdd");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-module.exports = sequelize;
+const db =async()=>{
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+} 
+module.exports = {sq: sequilize, db}
---
+++ models/inscription.jsx
@@ -0,0 +1,8 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../conf/database');
+
+const Users = sequelize.define('User', {
+ 
+});
+
+module.exports = Users;
