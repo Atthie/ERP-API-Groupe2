5fa5d1f65cbcec1a14e9f0c8df9f8f36246f2408
Atthie Mulongo <atthie27@gmail.com>
2023-06-06 08:23:29 +0200

    creation du modele USer avec sequelize

--- models/inscription.jsx
+++ models/inscription.jsx
@@ -1,8 +1,11 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../conf/database');
+const {sequelize}  = require('../conf/database');
 
 const Users = sequelize.define('User', {
- 
+    id:{type: DataTypes.INTEGER,primaryKey: true,autoIncrement: true,allowNull:false},
+    nom:{type: DataTypes.STRING,allowNull:false},
+    description:{type: DataTypes.STRING,allowNull:false},
+    email:{type: DataTypes.STRING,allowNull:false},
 });
 
 module.exports = Users;
--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
+const path=require('path');
 
-app.get('/',(req, res)=>{
-    res.send('Hello World');
-})
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set('views', path.join(__dirname, 'view'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('inscription');
+});
+app.post('/inscription', (req, res) => {
+    const { nom, email, motDePasse } = req.body;
+ 
+    res.send('Inscription réussie !');
+});
 
 
 app.listen(5000, ()=>{
