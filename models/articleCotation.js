5ea0f80205b3eaf63a3cb3712fd5e633d40f0f9e
Atthie Mulongo <atthie27@gmail.com>
2023-06-28 15:40:03 +0200

    creation du model articleCotation

---
+++ models/articleCotation.js
@@ -0,0 +1,44 @@
+'use strict';
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../config/database.js';
+import Article from './article.js';
+import DemandeCotation from './demandeCotation.js';
+
+
+class ArticleCotation extends Model {}
+
+ArticleCotation.init(
+  {
+   
+    quantite: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    dmId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: DemandeCotation,
+        key: 'id',
+      },
+    },
+    articleId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: Article,
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    modelName: 'ArticleCotation',
+  }
+);
+
+ArticleCotation.belongsTo(Article, { foreignKey: 'articleId' });
+ArticleCotation.belongsTo(DemandeCotation, { foreignKey: 'dmId' });
+
+
+export default ArticleCotation;
